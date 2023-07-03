d8c31cd1a280097473aaefadbcf617417d4dbf3d
Nik Ansell <gamecat69@hotmail.com>
2023-07-03 17:08:31 +0100

    added crazy stereo effect

--- daisyduino/simple-looper/simple-looper.ino
+++ daisyduino/simple-looper/simple-looper.ino
@@ -21,27 +21,27 @@ static const float kKnobMax = 1023;
 // Allocate buffer in SDRAM 
 static const uint32_t kBufferLengthSec = 5;
 static const uint32_t kSampleRate = 48000;
-static const size_t kBufferLenghtSamples = kBufferLengthSec * kSampleRate;
-static float DSY_SDRAM_BSS buffer[kBufferLenghtSamples];
+static const size_t kBufferLengthSamples = kBufferLengthSec * kSampleRate;
+static float DSY_SDRAM_BSS buffer_l[kBufferLengthSamples];
+static float DSY_SDRAM_BSS buffer_r[kBufferLengthSamples];
 
-static synthux::Looper looper;
+static synthux::Looper looper_l;
+static synthux::Looper looper_r;
 static PitchShifter pitch_shifter;
+static Chorus chorus;
 
 auto pitch_val = 0.5;
 
 void AudioCallback(float **in, float **out, size_t size) {
   for (size_t i = 0; i < size; i++) {
-    auto looper_out = looper.Process(in[1][i]);
-    //out[0][i] = out[1][i] = pitch_shifter.Process(looper_out);
-    out[0][i] = pitch_shifter.Process(looper_out);
-    // Test 1: Right channel is non-pitch shifted sample
-    //out[1][i] = looper_out;
-    // Test 2: Right channel is pitched up
-    pitch_val += 0.2;
-    set_pitch(pitch_val);
-    out[1][i] = pitch_shifter.Process(looper_out);
-    pitch_val -= 0.2;
-    set_pitch(pitch_val);
+    //auto looper_out = looper.Process(in[1][i]);
+    auto looper_out_l = looper_l.Process(in[0][i]);
+    auto looper_out_r = looper_l.Process(in[1][i]);
+    // Apply a pitch shift and chorus effect to only one channel
+    // This either creates a wild stereo effect, or a clean and effected channel for mixing together
+    auto pitch_shifter_out_l = pitch_shifter.Process(looper_out_l);
+    out[0][i] = chorus.Process(pitch_shifter_out_l);
+    out[1][i] = looper_out_r;
   }
 }
 
@@ -57,14 +57,19 @@ void setup() {
   float sample_rate = DAISY.get_samplerate();
 
   // Setup looper
-  looper.Init(buffer, kBufferLenghtSamples);
+  looper_l.Init(buffer_l, kBufferLengthSamples);
+  looper_r.Init(buffer_r, kBufferLengthSamples);
 
   // Setup pitch shifter
   pitch_shifter.Init(sample_rate);
 
+  // Setup Chorus
+  chorus.Init(sample_rate);
+
   // Setup pins
   pinMode(record_pin, INPUT);
 
+  // Start comms with Daisy hardware
   DAISY.begin(AudioCallback);
 }
 
@@ -72,7 +77,9 @@ void loop() {
   // Set loop parameters
   auto loop_start = fmap(analogRead(loop_start_pin) / kKnobMax, 0.f, 1.f);
   auto loop_length = fmap(analogRead(loop_length_pin) / kKnobMax, 0.f, 1.f, Mapping::EXP);
-  looper.SetLoop(loop_start, loop_length);
+  //looper.SetLoop(loop_start, loop_length);
+  looper_l.SetLoop(loop_start, loop_length);
+  looper_r.SetLoop(loop_start, loop_length);
 
   // Toggle record - Press and hold to record. Recording stops when button is released
   // Note for the Daisy Pod the button is the opposite, therefore the value is !record_on
@@ -81,7 +88,9 @@ void loop() {
   // auto record_on = digitalRead(record_pin);
   // Daisy Pod:
   auto record_on = !digitalRead(record_pin);
-  looper.SetRecording(record_on);
+  //looper.SetRecording(record_on);
+  looper_l.SetRecording(record_on);
+  looper_r.SetRecording(record_on);
   // Daisy Pod only, turn on LED when recording
   hw.leds[0].Set(record_on, 0, 0);
   
@@ -99,11 +108,8 @@ void loop() {
 
   // Clear loop if button 2 is pressed
   if (digitalRead(clear_loop_pin) == LOW) {
-    memset(buffer, 0, sizeof(float) * kBufferLenghtSamples);
-    hw.leds[1].Set(0, 255, 0);
-    System::Delay(100);
-    hw.leds[1].Set(0, 0, 0);
-    System::Delay(100);
+    memset(buffer_l, 0, sizeof(float) * kBufferLengthSamples);
+    memset(buffer_r, 0, sizeof(float) * kBufferLengthSamples);
     hw.leds[1].Set(0, 255, 0);
     System::Delay(100);
     hw.leds[1].Set(0, 0, 0);
